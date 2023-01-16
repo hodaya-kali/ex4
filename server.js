a62dc315f31b4569aa36ab8f4760a2ac80cc7f58
Avraham Weinstein <avrahamwn@gmail.com>
2023-01-16 23:54:45 +0200

    connect client to server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
-    fs = require('fs'),
-    cors = require('cors'),
-    routers = require('./server/routes/routes.js');
+    fs = require('fs')
+    //routers = require('./server/routes/routes.js');
 const port = 3001;
 
 const app=express();
@@ -31,7 +30,7 @@ app.use('/css', express.static(path.join(__dirname, 'client/css')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', routers);
+//app.use('/', routers);
 
 const server = app.listen(port, () => {
     console.log('listening on port %s...', server.address().port);
